a909c84d0d12bfc655194ac96d07670e92032e16
David <davillar.dv.321@gmail.com>
2026-05-26 16:22:01 -0300

    Agregue el endpoint tarea, que devuelve las tareas creadas

--- Backend/app.js
+++ Backend/app.js
@@ -8,6 +8,17 @@ app.get('/',(req,res) => {
     res.json({'mensaje':'servidor funcionando'});
 });
 
+tareas = [] //Array que se utiliza para guardar las tareas.
+
+//Endpoint get que muestra todas las tareas.
+app.get('/tarea',(req,res) => {
+    if(tareas.length===0){
+        res.status(200).json({'mensaje':'Aun no hay tareas'});
+    }else{
+        res.status(200).json(tareas);
+    }
+});
+
 app.listen(PORT,() => {
     console.log(`Servidor corriendo en localhost:${PORT}`);
 });
