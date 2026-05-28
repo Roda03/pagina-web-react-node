469ede6aa99d21eb864665c58d33cad6bce17f15
David <davillar.dv.321@gmail.com>
2026-05-28 06:28:22 -0300

    Retoques en el back

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
 const PORT=3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/',(req,res) => {
@@ -12,59 +14,53 @@ tareas = [] //Array que se utiliza para guardar las tareas.
 
 //Endpoint get que muestra todas las tareas.
 app.get('/tarea',(req,res) => {
-    if(tareas.length===0){
-        res.status(200).json({'mensaje':'Aun no hay tareas'});
-    }else{
-        res.status(200).json(tareas);
-    }
+    res.status(200).json(tareas);
 });
+
+//endpoint get que filtra una tarea con su respectivo id
+app.get('/tarea/:id',(req,res) => {
+    const id = req.params.id;
+    const tarea_id = tareas.find((elemento) => elemento.id===id);
+    if(!tarea_id){
+        res.status(404).json({'mensaje':'La tarea no existe'});
+    }else{
+        res.status(200).json(tarea_id);
+    }
+})
 
 //endpoint post que carga las tareas con el modelo adecuado
 app.post('/tarea', (req,res) => {
     const tarea = req.body;
     if(!tarea.title){
-        res.status(400).json({'mensaje':'Titulo no ingresado'});  
+        res.status(400).json(tarea);  
     }else{
-        tareas.push({
-        "id":(tareas.length+1),
+        const tareaCreada = {
+        "id":String(tareas.length+1),//Acabo de cambiar esto, por si algo falla despues
         "title":tarea.title,
         "description":tarea.description || "",
         "completed":false,
         "createdAt":new Date().toISOString()
-        })
-        res.status(201).json({'mensaje':'Datos ingresados correctamente'})
-    }
-})
-
-//endpoint get que filtra una tarea con su respectivo id
-app.get('/tarea/:id',(req,res) => {
-    const id = req.params.id;
-    const tarea_id = tareas.find((elemento) => elemento.id===parseInt(id));
-    if(!tarea_id){
-        res.status(404).json({'mensaje':'La tarea no existe'});
-    }else{
-        res.status(200).json({
-        'mensaje':'Tarea encontrada',
-        'tarea':tarea_id
-        });
+        }
+        tareas.push(tareaCreada);
+        res.status(201).json(tareaCreada);
     }
 })
 
 //endpoint delete que elimina una tarea por su id
 app.delete('/tarea/:id',(req,res) => {
-    const id = parseInt(req.params.id);
+    const id = req.params.id;
     const tarea_id = tareas.find((elemento) => elemento.id===id);
     if(!tarea_id){
         res.status(404).json({'mensaje':'La tarea no existe'});
     }else{
         tareas = tareas.filter((elemento) => elemento.id !== id);
-        res.status(200).json({'mensaje':'La tarea se elimino correctamente'});
+        res.status(200).json(tareas);
     }
 })
 
 //endpoint put que actualiza los datos de una tarea
 app.put('/tarea/:id',(req,res) => {
-    const id = parseInt(req.params.id);
+    const id = req.params.id;
     const tarea_id = tareas.find((elemento) => elemento.id===id);
     const tarea_actualizada = req.body;
     if(!tarea_id){
@@ -79,7 +75,7 @@ app.put('/tarea/:id',(req,res) => {
         if(tarea_actualizada.completed !== undefined){
             tarea_id.completed = tarea_actualizada.completed;
         }
-        res.status(200).json({'mensaje':'Se cambiaron correctamente los datos'});
+        res.status(200).json(tarea_id);
     };
 });
 
